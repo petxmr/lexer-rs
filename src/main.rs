34e7d7ffabc7c9c3d92a37962eb0d94fc196c9a9
petxmr <111649405+petxmr@users.noreply.github.com>
2023-12-20 16:08:09 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ enum Token {
 }
 
 fn lexer(input: &str) -> Vec<Token> {
-    let mut tokens = Vec::new();
+    let mut tokens: Vec<Token> = Vec::new();
 
     let identifier = Regex::new(r"^[a-zA-Z_][a-zA-Z0-9_]*").unwrap();
     let number = Regex::new(r"^\d+(\.\d+)?").unwrap();
@@ -19,7 +19,7 @@ fn lexer(input: &str) -> Vec<Token> {
     let parenthesis = Regex::new(r"^[(){}\[\]]").unwrap();
     let assignment = Regex::new(r"^=").unwrap();
 
-    let mut remaining = input.trim();
+    let mut remaining: &str = input.trim();
 
     while !remaining.is_empty() {
         if let Some(captures) = identifier.captures(remaining) {
@@ -27,7 +27,7 @@ fn lexer(input: &str) -> Vec<Token> {
             tokens.push(Token::Identifier(id.clone()));
             remaining = &remaining[id.len()..];
         } else if let Some(captures) = number.captures(remaining) {
-            let num = captures[0].parse::<f64>().unwrap();
+            let num: f64 = captures[0].parse::<f64>().unwrap();
             tokens.push(Token::Number(num));
             remaining = &remaining[captures[0].len()..];
         } else if let Some(captures) = operator.captures(remaining) {
@@ -35,7 +35,7 @@ fn lexer(input: &str) -> Vec<Token> {
             tokens.push(Token::Operator(op.clone()));
             remaining = &remaining[op.len()..];
         } else if let Some(captures) = parenthesis.captures(remaining) {
-            let parens = captures[0].chars().next().unwrap();
+            let parens: char = captures[0].chars().next().unwrap();
             tokens.push(Token::Parenthesis(parens));
             remaining = &remaining[captures[0].len()..];
         } else if let Some(captures) = assignment.captures(remaining) {
@@ -52,8 +52,8 @@ fn lexer(input: &str) -> Vec<Token> {
 }
 
 fn main() {
-    let input = "x = 10 + 5 * (3 - 2)";
-    let tokens = lexer(input);
+    let input: &str = "x = 10 + 5 * (3 - 2)";
+    let tokens: Vec<Token> = lexer(input);
 
     println!("Tokens: {:?}", tokens);
 }
